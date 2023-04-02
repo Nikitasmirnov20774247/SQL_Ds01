26907f45c514cfd41d090dc029ce016c3822e72b
Nikita <Nikitasmirnov2000@yandex.ru>
2023-04-03 02:24:02 +0300

    sql_file

---
+++ SQL_Ds01.sql
@@ -0,0 +1,13 @@
+USE sql_ds01;
+
+# 1 Создайте таблицу с мобильными телефонами, используя графический интерфейс. Заполните БД данными
+SELECT * FROM sql_ds01.phones;
+
+# 2 Выведите название, производителя и цену для товаров, количество которых превышает 2
+SELECT ProductName, Manufacturer, Price FROM sql_ds01.phones WHERE ProductCount > 2;
+
+# 3 Выведите весь ассортимент товаров марки "Samsung"
+SELECT * FROM sql_ds01.phones WHERE Manufacturer = 'Samsung';
+
+# 4 Выведите информацию о телефонах, где суммарный чек больше 100 000 и меньше 145 000
+SELECT * FROM sql_ds01.phones WHERE (Price * ProductCount) > 100000 AND (Price * ProductCount) < 145000;
